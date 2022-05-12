07382bbd85ed15cdc23470e88bc6556d30a98c98
denisomarcuyottito <182906@unsaac.edu.pe>
2022-05-12 06:52:58 -0500

    Add titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 const App = () => {
   return (
     <>
-      <span className="text-Blue text-3xl font-Poppins">Hola</span>
+      <div
+        role="application"
+        className="min-h-screen flex items-center justify-center"
+      >
+        <div className="flex flex-col">
+          <div className="font-Poppins text-center">
+            <h1 className="font-[200]">Reliable, efficient delivery</h1>
+            <h2 className="">Powered by Technology</h2>
+            <p>
+              Our Artificial Intelligence powered tools use millions of project
+              data points to ensure that your project is successful
+            </p>
+          </div>
+
+          <div>Card 1</div>
+          <div>Card 2</div>
+          <div>Card 3</div>
+          <div>Card 4</div>
+        </div>
+      </div>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
+      // Primary
       Red: "hsl(0, 78%, 62%)",
       Cyan: "hsl(180, 62 %, 55 %)",
       Orange: "hsl(34, 97%, 64%)",
       Blue: "hsl(212, 86%, 64%)",
+
+      // Neutral
       VeryDarkBlue: "hsl(234, 12%, 34%)",
       GrayishBlue: "hsl(229, 6%, 66%)",
       VeryLigthGray: "hsl(0, 0%, 98%)",
